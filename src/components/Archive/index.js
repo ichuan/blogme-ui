457a106bbc5ff787a773b9ddd7c7308d9c158ab2
ichuan <iyanchuan@gmail.com>
2020-01-03 16:54:38 +0800

    fix: archive

--- src/components/Archive/index.js
+++ src/components/Archive/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import Api from '../../utils/api';
-import useGlobal from '../../utils/hooks';
 import Pager from '../Pager';
 
 import './style.css';
@@ -31,11 +30,9 @@ const Box = ({ item }) => {
 export default () => {
   const [articles, setArticles] = useState([]);
   const [params, setParams] = useState({ limit: 100 });
-  const globalState = useGlobal()[0];
   useEffect(() => {
-    globalState.user &&
-      Api.get('/articles/archive', params).then(r => setArticles(r));
-  }, [params, globalState.user]);
+    Api.get('/articles/archive', params).then(r => setArticles(r));
+  }, [params]);
   return (
     <div className="container archive">
       <h1 className="title">文章索引</h1>
