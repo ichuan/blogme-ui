905b1354ef7a661f4bd2f08d9426c259c3ac8520
ichuan <iyanchuan@gmail.com>
2020-01-03 11:27:08 +0800

    fix: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default () => {
+  const year = new Date().getFullYear(),
+    hostname = window.location.hostname;
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>Copyright &copy; 2019 Blogme</p>
+        <p>
+          Copyright &copy; {year} {hostname}
+        </p>
       </div>
     </footer>
   );
