2b1e363637ce920fba1d948d309ec71caef19374
ichuan <iyanchuan@gmail.com>
2020-01-06 17:02:38 +0800

    feat: RSS Link; footer re-design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,13 +6,27 @@ export default () => {
     hostname = window.location.hostname;
   return (
     <footer className="footer">
-      <div className="content has-text-centered">
-        <p>
-          &copy; {year} {hostname} ·{' '}
-          <Link to="/archive" className="has-text-grey-dark">
-            文章索引
-          </Link>
-        </p>
+      <div className="level is-mobile container">
+        <div className="level-left">
+          <div className="level-item">
+            <p>
+              &copy; {year} {hostname}
+            </p>
+          </div>
+        </div>
+        <div className="level-right">
+          <div className="level-item">
+            <p>
+              <Link to="/archive" className="has-text-grey-dark">
+                文章索引
+              </Link>{' '}
+              ·{' '}
+              <a href="/feed" target="_blank" className="has-text-grey-dark">
+                RSS
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
